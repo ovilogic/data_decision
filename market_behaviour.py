f7513abb23f491f706d436c399f97b2caac6f7b6
Ovidiu Oprea <ovidiu.opreah@yahoo.com>
2026-04-14 23:33:32 +0100

    Learning to use yf.download and finding Adj Close is omitted by default

--- market_behaviour.py
+++ market_behaviour.py
@@ -1,4 +1,4 @@
-import yfinance
+import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,9 +6,14 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 
-apple = yfinance.Ticker('AAPL')
+# apple = yfinance.Ticker('AAPL')
 
-print(apple.info.get("longName"))
-print(apple.info.get("sector"))
-print(apple.info.get("industry"))
-print(apple.info.get("marketCap"))
+TICKERS = [
+    "AAPL", "MSFT", "GOOGL", "AMZN", "META",
+    "TSLA", "NVDA", "JPM", "V", "UNH",
+    "HD", "PG", "DIS", "BAC", "XOM",
+    "KO", "PEP", "INTC", "CSCO", "ORCL"
+]
+data = yf.download(tickers=TICKERS[0: 2], start="2023-01-01", end="2024-01-01", auto_adjust=False)
+print(data.columns)
+print(data["Close"])
