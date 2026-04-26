32e8633161e111daeb042c2e8cc65585364adc2d
Ovidiu Oprea <ovidiu.opreah@yahoo.com>
2026-04-26 12:13:06 +0300

    Created a copy of the server file (pipeline) so I can do quick testing on calculations that get sent to the frontend. In the future, I should create separate folders for produciton and dev. The pipeline.py now also sends averages of the features for each cluster. TODO: making use of rolling average to create a time series for returns.

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from price_features import SECTORS, build_features_df, compute_returns, compute_rolling_average, download_data
 from model import run_kmeans
+
 try:
     from flask_cors import CORS
     print("flask_cors imported successfully.")
@@ -13,17 +14,9 @@ CORS(app)
 @app.route("/api/clustered-stocks", methods=["POST"])
 def run_pipeline():
     input_data = request.get_json()
-
-    print("=== Incoming request payload ===")
-    print(input_data)
-
     sector_id = input_data.get("sector")
     end_year = input_data.get("year")
 
-    print("Sector ID:", sector_id)
-    print("End Year:", end_year)
-
-    # sector_id = "3" # For testing purposes, we can hardcode the sector_id to "1" (Tech)
     if sector_id not in SECTORS:
         return jsonify({"error": "Invalid sector"}), 400
     
@@ -32,24 +25,34 @@ def run_pipeline():
 
     data = download_data(tickers)
     returns_df = compute_returns(data)
+
     rolling_avg_df = compute_rolling_average(returns_df)
     features_df = build_features_df(data).dropna()
+
     clustered_df, model, scaler = run_kmeans(features_df)
     # Export to csv
     # clustered_df.to_csv("clustered_stocks.csv", index=True)
     # rolling_avg_df.to_csv("rolling_avg_stocks.csv", index=True)
-    
+
     clustered_df = clustered_df.round(6) # Round the values to 6 decimal places for better readability
     company_map = SECTORS[sector_id]["companies"]
     clustered_df["company"] = clustered_df.index.map(company_map)
+
+    avg_ret_clustered = clustered_df.groupby("cluster")["average_returns"].mean()
+    vol_clustered = clustered_df.groupby("cluster")["volatility"].mean()
+    max_dd_clustered = clustered_df.groupby("cluster")["max_drawdown"].mean()
    
     return jsonify({
     "sector": selected_sector["name"],
     "year": end_year,
-    "clusters": clustered_df.to_dict(orient="index")
+    "clusters": clustered_df.to_dict(orient="index"),
+    "cluster_averages": {
+        "avg_returns": avg_ret_clustered.to_dict(),
+        "avg_volatility": vol_clustered.to_dict(),
+        "avg_max_drawdown": max_dd_clustered.to_dict()
+    }
     })
 
 if __name__ == "__main__":
-    # run_pipeline()
     app.run(debug=True)
    
